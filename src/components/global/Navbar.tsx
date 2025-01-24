9d192ee452193b4ad211d08f7d1c821f19b06826
JUSTMEETPATEL <justmeetpatel@gmail.com>
2025-01-24 18:08:52 +0530

    Navbar auth options added

--- src/components/global/Navbar.tsx
+++ src/components/global/Navbar.tsx
@@ -1,7 +1,18 @@
+'use client';
+
 import { Link } from 'lucide-react';
 import React from 'react';
+import { Button } from '../ui/button';
+import { authClient, useSession } from '@/lib/auth-client';
+import { redirect } from 'next/navigation';
+
+const handleClick = () => {
+  authClient.signOut();
+  redirect('/sign-in');
+};
 
 const Navbar = () => {
+  const session = useSession();
   return (
     <div>
       <nav className="bg-black text-white p-4 absolute top-0 left-0 right-0 z-10">
@@ -27,6 +38,40 @@ const Navbar = () => {
                 </Link>
               </li>
             ))}
+            <li>
+              {session.data?.user.email ? (
+                <div>
+                  <Button
+                    onClick={handleClick}
+                    variant="ghost"
+                    size="lg"
+                    className="bg-gray-700 text-white border-gray-600 hover:bg-gray-600 hover:text-white hover:border-gray-500 hover:shadow-md transition-all duration-300 ease-in-out transform hover:scale-105"
+                  >
+                    Sign Out
+                  </Button>
+                </div>
+              ) : (
+                <div className="lg:space-x-2 flex items-center justify-center">
+                  <Link href="/sign-in">
+                    <Button
+                      variant="ghost"
+                      size="lg"
+                      className="bg-gray-700 text-white border-gray-600 hover:bg-gray-600 hover:text-white hover:border-gray-500 hover:shadow-md transition-all duration-300 ease-in-out transform hover:scale-105"
+                    >
+                      Sign In
+                    </Button>
+                  </Link>
+                  <Link href="/sign-up">
+                    <Button
+                      size="lg"
+                      className="bg-emerald-600 text-white hover:bg-emerald-500 hover:shadow-lg transition-all duration-300 ease-in-out transform hover:scale-105"
+                    >
+                      Apply Now
+                    </Button>
+                  </Link>
+                </div>
+              )}
+            </li>
           </ul>
         </div>
       </nav>
