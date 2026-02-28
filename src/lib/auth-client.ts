a9943bb7162a8b47c434c54fba15cdf91f3164f0
Varun Sinha <varunsinha2604@gmail.com>
2026-03-01 04:34:24 +0530

    build-fix

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -4,7 +4,6 @@ export const authClient = createAuthClient({
   baseURL: process.env.BETTER_AUTH_URL,
 });
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
 export const {
   signIn,
   signUp,
@@ -12,4 +11,5 @@ export const {
   useSession,
   forgetPassword,
   resetPassword,
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
 } = authClient as any;
